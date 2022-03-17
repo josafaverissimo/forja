ccf89565a8e9288a3b78a15d6626ea883c119afa
josafa <josafaverissimo98@gmail.com>
2022-03-17 19:44:34 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>CSV</title>
+</head>
+<body>
+    <label for="input-csv">CSV: </label>
+    <input id="input-csv" type="file">
+</body>
+</html>
