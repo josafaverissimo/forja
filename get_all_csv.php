d4c684144673f7594f779a78962971cc7e462c72
josafa <josafaverissimo98@gmail.com>
2022-03-20 18:49:00 -0300

    get all csv file content

---
+++ get_all_csv.php
@@ -0,0 +1,26 @@
+<?php
+    $uploadDirectory = './uploads';
+    $csvFiles = array_slice(scandir($uploadDirectory), 2);
+    $csvFilesContentByFilename = [];
+
+    foreach($csvFiles as $file):
+        $filename = explode('.', $file)[0];
+        $filepath = "{$uploadDirectory}/$file";
+
+        $handle = fopen($filepath, 'r');
+        $content = [];
+
+        while(!feof($handle)):
+            $line = str_replace(["\n", "\r"], "", fgets($handle));
+
+            if($line === "") continue;
+            
+            $content[] = $line;
+
+        endwhile;
+        
+        $csvFilesContentByFilename[$filename] = $content;
+    endforeach;
+    
+    header('Content-Type: application/json');
+    echo json_encode($csvFilesContentByFilename);
--- upload.php
+++ upload.php
@@ -8,12 +8,4 @@
 
     endif;
 
-    /*Check file type*/
-    if($uploadDir[sizeof($uploadDir)-1] != 'csv'){
-        echo 'Arquivo não suportado';
-    }
-    else{
-        echo 'Upload feito com sucesso';
-        move_uploaded_file($_FILES['file']['tmp_name'], $filePath);
-    }
-?>
+    move_uploaded_file($_FILES['file']['tmp_name'], $filePath);
