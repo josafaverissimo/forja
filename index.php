6c5bb7f588473ed3e478bc6186ff21649814800a
josafa <josafaverissimo98@gmail.com>
2022-03-20 21:41:29 -0300

    show csv data

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    $baseUrl = "http://localhost/github/forja";
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,5 +15,28 @@
 
         <button type="submit">Submit</button>
     </form>
+
+    <hr>
+
+    <div id="csv-files">
+        <div class="csv-file-table-boilerplate" hidden>
+            <h1></h1>
+
+            <table border="1">
+                <thead>
+                    <tr></tr>
+                </thead>  
+                <tbody></tbody>
+            </table>
+        </div>
+    </div>
+
+    <script src="<?php echo "$baseUrl/scripts/show_csv.js"; ?>"></script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', event => {
+            showCsvFiles()
+        })
+    </script>
 </body>
 </html>
---
+++ scripts/show_csv.js
@@ -0,0 +1,41 @@
+function getCsvContent() {
+    return fetch("http://localhost/github/forja/get_all_csv.php")
+        .then(response => response.json())
+}
+
+async function showCsvFiles() {
+    const csvContent = await getCsvContent()
+    const csvFilesContainer = document.getElementById("csv-files")
+
+    Object.keys(csvContent).forEach(key => {
+        const csvFilename = key
+        const csvHeader = csvContent[key][0].split(',')
+        const csvBody = csvContent[key].slice(1, csvContent[key].length)
+
+        const csvTableBoilerplate = csvFilesContainer.querySelector('.csv-file-table-boilerplate').cloneNode(true)
+
+        csvTableBoilerplate.querySelector('h1').textContent = csvFilename;
+
+        csvHeader.forEach(cell => {
+            const th = document.createElement('th')
+            th.textContent = cell
+            csvTableBoilerplate.querySelector('table thead tr').appendChild(th)
+        })
+
+        csvBody.forEach(row => {
+            const tr = document.createElement('tr')
+
+            row.split(',').forEach(cell => {
+                const td = document.createElement('td')
+                td.textContent = cell
+
+                tr.appendChild(td)
+            })
+
+            csvTableBoilerplate.querySelector('table tbody').appendChild(tr)
+        })
+
+        csvFilesContainer.appendChild(csvTableBoilerplate)
+        csvTableBoilerplate.removeAttribute('hidden')
+    })
+}
